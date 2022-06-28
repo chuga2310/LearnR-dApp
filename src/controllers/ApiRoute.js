503ea8c66dae1a0d07c017ec6b6855a87a897c45
cuonglm22 <cuonglm22@fpt.com.vn>
2022-06-28 10:10:54 +0700

    initial commit

--- src/controllers/ApiRoute.js
+++ src/controllers/ApiRoute.js
@@ -18,199 +18,197 @@ let firstAccount;
 let client;
 let signingClient;
 
-const getWallet = async () => {
-  const wallet = await DirectSecp256k1HdWallet.fromMnemonic(mnemonic, { prefix: 'aura' });
-  return wallet;
+const getWallet = async() => {
+    const wallet = await DirectSecp256k1HdWallet.fromMnemonic(mnemonic, { prefix: 'aura' });
+    return wallet;
 }
 
-const get1stAccount = async (wallet) => {
-  const [firstAccount] = await wallet.getAccounts();
-  return firstAccount;
+const get1stAccount = async(wallet) => {
+    const [firstAccount] = await wallet.getAccounts();
+    return firstAccount;
 }
 
-const getAuraWasmClient = async () => {
-  const client = await CosmWasmClient.connect(rpcEndpoint);
-  return client;
+const getAuraWasmClient = async() => {
+    const client = await CosmWasmClient.connect(rpcEndpoint);
+    return client;
 }
 
-const getSigningAuraWasmClient = async (wallet) => {
-  const signingClient = await SigningCosmWasmClient.connectWithSigner(rpcEndpoint, wallet);
-  return signingClient;
+const getSigningAuraWasmClient = async(wallet) => {
+    const signingClient = await SigningCosmWasmClient.connectWithSigner(rpcEndpoint, wallet);
+    return signingClient;
 }
+
 function makeFileObjects(img) {
-  // You can create File objects from a Buffer of binary data
-  var buffer = Buffer.from(img.data, 'base64');
+    // You can create File objects from a Buffer of binary data
+    var buffer = Buffer.from(img.data, 'base64');
 
-  const files = [
-    new File(['contents-of-file-1'], 'plain-utf8.txt'),
-    new File([buffer], img.name)
-  ]
-  return files
+    const files = [
+        new File(['contents-of-file-1'], 'plain-utf8.txt'),
+        new File([buffer], img.name)
+    ]
+    return files
 }
 
-router.route('/Image/Upload').post(async (req, res) => {
-  /* 	#swagger.tags = ['Image']
-  #swagger.description = 'Upload image to IPFS' */
+router.route('/Image/Upload').post(async(req, res) => {
+    /* 	#swagger.tags = ['Image']
+    #swagger.description = 'Upload image to IPFS' */
 
-  /*    #swagger.consumes = ['multipart/form-data']  
-      #swagger.parameters['image'] = {
-          in: 'formData',
-          type: 'file',
-          required: 'true',
-          description: 'Some description...',
-  } */
+    /*    #swagger.consumes = ['multipart/form-data']  
+        #swagger.parameters['image'] = {
+            in: 'formData',
+            type: 'file',
+            required: 'true',
+            description: 'Some description...',
+    } */
 
-  const imageFile = req.files.image;
+    const imageFile = req.files.image;
 
-  try {
-    const filesObj = makeFileObjects(imageFile);
-    const cid = await storage.put(filesObj);
-    console.log('Content added with CID:', cid);
-    const metadataGatewayURL = makeGatewayURL(cid, 'metadata.json');
-    const imageGatewayURL = makeGatewayURL(cid, imageFile.name);
-    const imageURI = `ipfs://${cid}/${imageFile.name}`;
-    const metadataURI = `ipfs://${cid}/metadata.json`;
+    try {
+        const filesObj = makeFileObjects(imageFile);
+        const cid = await storage.put(filesObj);
+        console.log('Content added with CID:', cid);
+        const metadataGatewayURL = makeGatewayURL(cid, 'metadata.json');
+        const imageGatewayURL = makeGatewayURL(cid, imageFile.name);
+        const imageURI = `ipfs://${cid}/${imageFile.name}`;
+        const metadataURI = `ipfs://${cid}/metadata.json`;
 
-    res.status(200).json({
-      data: [{ cid, metadataGatewayURL, imageGatewayURL, imageURI, metadataURI }],
-      message: 'Upload Result'
-    });
-  } catch (err) {
-    res.status(500).json({
-      data: [err.message],
-      message: 'Error'
-    });
-  }
+        res.status(200).json({
+            data: [{ cid, metadataGatewayURL, imageGatewayURL, imageURI, metadataURI }],
+            message: 'Upload Result'
+        });
+    } catch (err) {
+        res.status(500).json({
+            data: [err.message],
+            message: 'Error'
+        });
+    }
 })
 
-router.route('/Token/Mint').post(async (req, res) => {
-  /* 	#swagger.tags = ['Token']
-  #swagger.description = 'Mint NFT Token' */
+router.route('/Token/Mint').post(async(req, res) => {
+    /* 	#swagger.tags = ['Token']
+    #swagger.description = 'Mint NFT Token' */
 
-  /*  #swagger.parameters['obj'] = {
-          in: 'body',
-          description: 'Token Information',
-          schema: { $ref: '#/definitions/Mint' }
-  } */
+    /*  #swagger.parameters['obj'] = {
+            in: 'body',
+            description: 'Token Information',
+            schema: { $ref: '#/definitions/Mint' }
+    } */
 
-  if (!wallet) {
-    wallet = await getWallet();
-  }
-  if (!firstAccount) {
-    firstAccount = await get1stAccount(wallet);
-  }
-  if (!signingClient) {
-    signingClient = await getSigningAuraWasmClient(wallet);
-  }
-
-  const mintMsg = {
-    mint: {
-      token_id: req.body.token_id,
-      owner: req.body.owner,
-      token_uri: req.body.token_uri,
-      extension: req.body.extension
+    if (!wallet) {
+        wallet = await getWallet();
     }
-  };
+    if (!firstAccount) {
+        firstAccount = await get1stAccount(wallet);
+    }
+    if (!signingClient) {
+        signingClient = await getSigningAuraWasmClient(wallet);
+    }
 
-  const fee = {
-    amount: [
-      {
-        denom: 'uaura',
-        amount: '16',
-      },
-    ],
-    gas: '152375',
-  }
+    const mintMsg = {
+        mint: {
+            token_id: req.body.token_id,
+            owner: req.body.owner,
+            token_uri: req.body.token_uri,
+            extension: req.body.extension
+        }
+    };
 
-  try {
-    const result = await signingClient.execute(firstAccount.address, contractAddress, mintMsg, fee);
-    res.status(200).json({
-      data: [result],
-      message: 'Mint Result'
-    });
-  } catch (err) {
-    res.status(500).json({
-      data: [err.message],
-      message: 'Error'
-    });
-  }
+    const fee = {
+        amount: [{
+            denom: 'uaura',
+            amount: '16',
+        }, ],
+        gas: '152375',
+    }
+
+    try {
+        const result = await signingClient.execute(firstAccount.address, contractAddress, mintMsg, fee);
+        res.status(200).json({
+            data: [result],
+            message: 'Mint Result'
+        });
+    } catch (err) {
+        res.status(500).json({
+            data: [err.message],
+            message: 'Error'
+        });
+    }
 })
 
-router.route('/Token/Get/:id').get(async (req, res) => {
-  /* 	#swagger.tags = ['Token']
-  #swagger.description = 'Mint NFT Token' */
+router.route('/Token/Get/:id').get(async(req, res) => {
+    /* 	#swagger.tags = ['Token']
+    #swagger.description = 'Mint NFT Token' */
 
-  if (!client) {
-    client = await getAuraWasmClient();
-  }
-  const nftInfo = {
-    nft_info: {
-      token_id: String(req.params.id)
+    if (!client) {
+        client = await getAuraWasmClient();
     }
-  }
-  try {
-    const result = await client.queryContractSmart(contractAddress, nftInfo);
-    res.status(200).json({
-      data: [result],
-      message: 'Found Result'
-    });
-  } catch (err) {
-    res.status(500).json({
-      data: [err.message],
-      message: 'Error'
-    });
-  }
+    const nftInfo = {
+        nft_info: {
+            token_id: String(req.params.id)
+        }
+    }
+    console.log(client)
+    try {
+        const result = await client.queryContractSmart(contractAddress, nftInfo);
+        res.status(200).json({
+            data: [result],
+            message: 'Found Result'
+        });
+    } catch (err) {
+        console.log(err)
+        res.status(500).json({
+            data: [err.message],
+            message: 'Error'
+        });
+    }
 })
 
-router.post('/Token/Transfer', async (req, res, next) => {
-  /* 	#swagger.tags = ['Token']
-      #swagger.description = 'Transfer NFT Token' */
+router.post('/Token/Transfer', async(req, res, next) => {
+    /* 	#swagger.tags = ['Token']
+        #swagger.description = 'Transfer NFT Token' */
 
-  /*  #swagger.parameters['obj'] = {
-          in: 'body',
-          description: 'Transfer Information',
-          schema: { $ref: '#/definitions/Transfer' }
-  } */
+    /*  #swagger.parameters['obj'] = {
+            in: 'body',
+            description: 'Transfer Information',
+            schema: { $ref: '#/definitions/Transfer' }
+    } */
 
-  if (!wallet) {
-    wallet = await getWallet();
-  }
-  if (!firstAccount) {
-    firstAccount = await get1stAccount(wallet);
-  }
-  if (!signingClient) {
-    signingClient = await getSigningAuraWasmClient(wallet);
-  }
-  const transMsg = {
-    transfer_nft:
-    {
-      token_id: req.body.token_id,
-      recipient: req.body.recipient,
+    if (!wallet) {
+        wallet = await getWallet();
     }
-  }
+    if (!firstAccount) {
+        firstAccount = await get1stAccount(wallet);
+    }
+    if (!signingClient) {
+        signingClient = await getSigningAuraWasmClient(wallet);
+    }
+    const transMsg = {
+        transfer_nft: {
+            token_id: req.body.token_id,
+            recipient: req.body.recipient,
+        }
+    }
 
-  const fee = {
-    amount: [
-      {
-        denom: 'uaura',
-        amount: '1000',
-      },
-    ],
-    gas: '152375',
-  }
+    const fee = {
+        amount: [{
+            denom: 'uaura',
+            amount: '1000',
+        }, ],
+        gas: '152375',
+    }
 
-  try {
-    const result = await signingClient.execute(firstAccount.address, contractAddress, transMsg, fee);
-    res.status(200).json({
-      data: [result],
-      message: 'Tranfer Result'
-    });
-  } catch (err) {
-    res.status(500).json({
-      data: [err.message],
-      message: 'Error'
-    });
-  }
+    try {
+        const result = await signingClient.execute(firstAccount.address, contractAddress, transMsg, fee);
+        res.status(200).json({
+            data: [result],
+            message: 'Tranfer Result'
+        });
+    } catch (err) {
+        res.status(500).json({
+            data: [err.message],
+            message: 'Error'
+        });
+    }
 })
 
 export default router
